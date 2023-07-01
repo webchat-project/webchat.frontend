d41cfee690f037b45e25a956eb356a52c3d52935
GledianMeta <117811732+GledianMeta@users.noreply.github.com>
2023-07-01 12:20:33 +0200

    emoji

--- src/components/main/messages/MessageInputBox.jsx
+++ src/components/main/messages/MessageInputBox.jsx
@@ -1,33 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Picker from 'emoji-picker-react';
+import { BsEmojiSmileFill } from 'react-icons/bs';
 
 export default function MessageInputBox({ handleSubmit }) {
+  // Messaggio di input
+  const [messageInput, setMessageInput] = useState('');
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
-    // Messaggio di input
-    const [messageInput, setMessageInput] = useState("")
+  // Metodo per l'invio del messaggio
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    handleSubmit(messageInput);
+    setMessageInput('');
+  };
 
-    // Metodo per l'invio del messaggio
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-        handleSubmit(messageInput);
-        setMessageInput('');
-    };
+  const handleEmojiPickerHideShow=()=>{
+    setShowEmojiPicker(!showEmojiPicker);
+  }
 
-    return (
-        <div id="message-input-container">
-            <form onSubmit={handleFormSubmit} id="form-input-container">
-                <input
-                    id="message-input"
-                    type='text'
-                    value={messageInput}
-                    placeholder='Scrivi un messaggio'
-                    onChange={e => setMessageInput(e.target.value)}>
-                </input>
-                <button id="message-sender" disabled={messageInput.trim() === ""}>
-                    <span className="material-symbols-outlined">
-                        send
-                    </span>
-                </button>
-            </form>
-        </div >
-    )
+  const handleEmojiClick = (event)=>{
+    //let message = messageInput;
+   // message += event.emoji;
+    setMessageInput(preValue => preValue += event.emoji );
+  }
+
+
+  return (
+    
+    
+    <div id="message-input-container">
+        
+      <form onSubmit={handleFormSubmit} id="form-input-container">
+      <BsEmojiSmileFill className="emoji-button" onClick={handleEmojiPickerHideShow}/>
+      {showEmojiPicker &&  <Picker className="emoji-picker"  onEmojiClick={handleEmojiClick} /> }
+        <input
+          id="message-input"
+          type="text"
+          value={messageInput}
+          placeholder="Scrivi un messaggio"
+          onChange={e => setMessageInput(e.target.value)}
+        ></input>
+        <button id="message-sender" disabled={messageInput.trim() === ''}>
+          <span className="material-symbols-outlined">send</span>
+        </button>
+      </form>
+    </div>
+
+  );
 }
