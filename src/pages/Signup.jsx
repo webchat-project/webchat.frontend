8b2d477e36e0192515aa1a924cb87359233fd4c2
salvatoregesualdo <salvigesualdo@libero.it>
2023-07-02 18:19:15 +0200

    fix

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -56,7 +56,7 @@ export default function Signup() {
 
     } catch (error) {
       console.log(error);
-      setError(error.message)
+      setError(error.response.data.error)
     }
     setLoading(false);
   };
